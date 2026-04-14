5102de416247b2a0c0109b940d74bf6f1a33cf29
Luis Pedro Gudiel <luispedrogudiel@MacBook-Air-de-Luis.local>
2026-04-13 20:54:54 -0600

    cambios index 5

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'examen-v1';
+const CACHE = 'examen-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
